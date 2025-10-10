03437bba14253c53d05d3da4b0bf2f00714d96fe
Bhumika <bhumika04shimla@gmail.com>
2025-10-10 21:49:31 +0530

    made the DigitalClock fully like practice

--- Digital-Clock/src/Components/DigitalClock.jsx
+++ Digital-Clock/src/Components/DigitalClock.jsx
@@ -1,25 +1,59 @@
+import { useEffect, useState } from "react"
+
 const DigitalClock = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return() => {clearInterval(interval)};
+  }, [])
+
+  const formatTimeDigits = (date) => {
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+
+    return{
+      hours: hours.split(""),
+      minutes: minutes.split(""),
+      seconds: seconds.split("")
+    }
+  }
+
+  const timeDigits = formatTimeDigits(time);
+
   return (
     <>
     <div className="flip-clock-container d-flex justify-content-center align-items-center vh-100">
+      
       <div className="flip-clock">
         {/* Hours */}
-        <div className="panel"><span className="number">1</span></div>
-        <div className="panel"><span className="number">0</span></div>
+
+        {timeDigits.hours.map((num, i) => (
+          <div className="panel" key={i}><span className="number" key={`hours${i}`}>{num}</span></div>
+      ))}
 
         {/* Separator */}
         <div className="separator">:</div>
 
         {/* Minutes */}
-        <div className="panel"><span className="number">0</span></div>
-        <div className="panel"><span className="number">0</span></div>
+
+        {timeDigits.minutes.map((num, i) => (
+          <div className="panel" key={i}><span className="number" key={`minutes${i}`}>{num}</span></div>
+      ))}
 
         {/* Separator */}
         <div className="separator">:</div>
 
         {/* Seconds */}
-        <div className="panel"><span className="number">0</span></div>
-        <div className="panel"><span className="number">0</span></div>
+
+        {timeDigits.seconds.map((num, i) => (
+          <div className="panel" key={i}><span className="number" key={`seconds${i}`}>{num}</span></div>
+      ))}
+
       </div>
     </div>
 
